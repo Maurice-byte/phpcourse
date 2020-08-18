691950966ee11657f69439344e1abf45f004a080
Maurice Osuji <osujimaurice3@gmail.com>
2020-08-17 23:02:15 -0700

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,3 @@
+ <?php 
+      echo "Copyright(c) " . $company_name ." " . $tagline . " " . date('Y') . " - All Rights Reserved";
+ ?>
---
+++ variables.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+$company_name = "St Joseph De IT Workers, Inc.";
+$tagline = "Teaching ICT since ";
+
+
+
+
+
+
+
+
+
+
+
+ ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
